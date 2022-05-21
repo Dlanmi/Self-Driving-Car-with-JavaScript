97d412896855b0ca571ac698af3bc4348170b8c6
Dlanmi <danielgamier1@gmail.com>
2022-05-21 15:23:35 -0500

    Implementando los sensores para la deteccion

--- car.js
+++ car.js
@@ -13,11 +13,13 @@ class Car {
     // Angulo
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
-  update() {
+  update(roadBorders) {
     this.#move();
+    this.sensor.update(roadBorders);
   }
 
   #move() {
@@ -72,5 +74,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
